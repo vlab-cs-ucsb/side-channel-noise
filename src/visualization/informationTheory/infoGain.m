e1a04f357637fc11eb4ef63adf8604ea7065737c
Lucas Bang <lucasbang@gmail.com>
2016-10-31 13:28:50 -0700

    Initial commit of all source files and test data.

---
+++ src/visualization/informationTheory/infoGain.m
@@ -0,0 +1,16 @@
+function [infogain expected_info_gain]= infoGain(P, class_probs)
+P = class_probs' .* P;
+P_cond = (P./ sum(P));
+
+
+H_init = log2(size(P,1));
+
+for i = 1:length(P) - 1;
+	H_class_obs(i) = entropy(P_cond(:,i)');
+end
+
+infogain = H_init - H_class_obs;
+infogain( isnan(infogain) ) = 0;
+
+expected_info_gain = sum(P(:,1:end-1))*infogain';
+
